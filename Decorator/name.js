312f8424327e1ba0af4b1403872ae8a0e3c83ad1
Romansurgut1986 <49689874+Romansurgut1986@users.noreply.github.com>
2019-06-25 13:49:59 +0300

    Add files via upload

---
+++ Decorator/name.js
@@ -0,0 +1,7 @@
+//Задача 1
+function Lazyman () {alert ('Кто хочет поработать?')};
+
+function Lazyman() {
+    console.log('Функция выполниться с задержкой в 1 секунду!')}  
+    let paused = pause(Lazyman, 1);
+    paused();
