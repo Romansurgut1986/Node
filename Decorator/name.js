78a70cf0034589bf056601c0023a95922e9d27ed
Romansurgut1986 <49689874+Romansurgut1986@users.noreply.github.com>
2019-06-28 15:55:40 +0300

    Add files via upload

--- Decorator/name.js
+++ Decorator/name.js
@@ -1,7 +1,14 @@
-//Задача 1
-function Lazyman () {alert ('Кто хочет поработать?')};
+function cheater(function1, seconds) {
+    function Lazyman() {
+        alert('Кто хочет поработать?');
+        setTimeout(() => function1.call(this, arguments), seconds * 2000);
+    }
+    return Lazyman;
+};
 
-function Lazyman() {
-    console.log('Функция выполниться с задержкой в 1 секунду!')}  
-    let paused = pause(Lazyman, 1);
-    paused();
+function func() {
+    alert('Дело было не в бобине, дело было не в реле, дело было в той хорошей женщине, что сидела на руле!!!');
+}
+let paused = cheater(func, 3);
+paused ();
+      
